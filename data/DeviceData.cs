8c55b77d470af2e45794386495570de8b4a92da2
ritvikshekhar <ritvik23440@iiitd.ac.in>
2025-07-06 16:17:49 +0530

    new changes made according to need

--- data/DeviceData.cs
+++ data/DeviceData.cs
@@ -1,12 +1,20 @@
-﻿namespace project.data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.data
 {
     public class DeviceData
     {
         public int DeviceID { get; set; }
+
+        [Required]
         public string SerialNumber { get; set; }
+
+        [Required]
         public string DeviceName { get; set; }
+
+        [Required]
         public string SupportLabelIdentifier { get; set; }
+
         public DateTime UpdatedAt { get; set; }
     }
-
 }
